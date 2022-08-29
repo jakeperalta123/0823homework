d2df5feb85811f175e23be909a764c9b67de08bf
jakeperalta123 <iloveheat1105@gmail.com>
2022-08-29 23:38:31 +0800

    Second commit

--- javaFrame.java
+++ javaFrame.java
@@ -61,19 +61,19 @@ public class javaFrame {
             System.out.println(k + ": " + String.join(", ", v));
         });
 
-        // Map<String, String> capitalMap = new HashMap<>();
-        // capitalMap.put("USA", "Washington");
-        // capitalMap.put("Japan", "Tokyo");
-        // capitalMap.put("Thailand", "Bangkok");
-        // capitalMap.put("UK", "London");
-        // capitalMap.put("Australia", "Canberra");
-        // capitalMap.put("Denmark", "Copenhagen");
-        // capitalMap.put("Egypt", "Cairo");
-        // capitalMap.put("Vietnam", "Hanoi");
-        // capitalMap.put("Italy", "Rome");
-        // capitalMap.put("Brazil", "Brazilia");
-        // for (Map.Entry<String, String> entry : capitalMap.entrySet()) {
-        //     System.out.println(entry.getKey()+" : "+entry.getValue());
-        // }
+        Map<String, String> capitalMap = new HashMap<>();
+        capitalMap.put("USA", "Washington");
+        capitalMap.put("Japan", "Tokyo");
+        capitalMap.put("Thailand", "Bangkok");
+        capitalMap.put("UK", "London");
+        capitalMap.put("Australia", "Canberra");
+        capitalMap.put("Denmark", "Copenhagen");
+        capitalMap.put("Egypt", "Cairo");
+        capitalMap.put("Vietnam", "Hanoi");
+        capitalMap.put("Italy", "Rome");
+        capitalMap.put("Brazil", "Brazilia");
+        for (Map.Entry<String, String> entry : capitalMap.entrySet()) {
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
     }
 }
